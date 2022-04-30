112555c94fe7d7f68bc133604c79b4f163be0e9d
nabcellent <nabcellent.dev@gmail.com>
2022-05-01 00:08:51 +0300

    Implementing authentication
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/components/license/Renew.js
+++ src/components/license/Renew.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Renew = () => {
-  return (
-    <div className="max-w-xs mx-auto p-5">
-      <div className="border w-full bg-green-400 rounded-lg cursor-pointer py-3">
-        <h1 className="text-2xl text-center font-semibold text-white">Renew your License</h1>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="max-w-xs mx-auto p-5">
+            <div className="border w-full bg-green-400 rounded-lg cursor-pointer py-3">
+                <h1 className="text-2xl text-center font-semibold text-white">Renew your License</h1>
+            </div>
+        </div>
+    );
+};
 
-export default Renew
+export default Renew;
