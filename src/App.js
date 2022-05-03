c80b8ff16d6accc657c7b1e082935e50d7c7ad79
lbnmahs <66216041+lbnmahs@users.noreply.github.com>
2022-05-03 22:52:02 +0300

    Added files for the different modules. I am just being a good samaritan. Lol

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Layout from './components/Layout';
 import Apply from './components/license/Apply'
 import Renew from './components/license/Renew'
 import { Routes, Route } from 'react-router-dom';
+import MyDl from './components/accountInfo/MyDl';
+import AccManagement from './components/accountInfo/AccManagement';
+import PdlApplication from './components/newDrivers/PdlApplication';
+import TestBooking from './components/newDrivers/TestBooking';
 
 function App() {
     return (
@@ -11,14 +15,14 @@ function App() {
                 <Route path="/" element={<h1>WELCOME</h1>}/>
                 <Route path="/home" element={<h1>TO ARENA</h1>}/>
                 {/* Account Info */}
-                <Route path="/my-dl" element={<h1>MY DL</h1>}/>
-                <Route path="/account-management" element={<h1>ACCOUNT MANAGEMENT</h1>}/>
+                <Route path="/my-dl" element={<MyDl />}/>
+                <Route path="/account-management" element={<AccManagement />}/>
                 {/* License */}
                 <Route path="/apply-for-smart-dl" element={<Apply />} />
                 <Route path="/renew-dl" element={<Renew />} />
                 {/* New Drivers */}
-                <Route path="/pdl-application" element={<h1>APPLICATION FOR PDL</h1>}/>
-                <Route path="/test-booking" element={<h1>TEST BOOKING</h1>}/>
+                <Route path="/pdl-application" element={<PdlApplication />}/>
+                <Route path="/test-booking" element={<TestBooking />}/>
             </Routes>
         </Layout>
     );
