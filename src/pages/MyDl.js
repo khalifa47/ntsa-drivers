32ba5f39c20f632d35e078e071b07dc93eb4e34d
khalifa47 <khalifafumo5@gmail.com>
2022-05-09 23:33:12 +0300

    grid setup with theme

--- src/pages/MyDl.js
+++ src/pages/MyDl.js
@@ -1,12 +1,17 @@
-import { useTheme } from "@mui/material";
 import { useAuth } from '../hooks/useAuth';
-import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { findUserById } from '../redux/features/users/usersSlice';
+import { Card } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const CardPro = styled(Card)(({ theme }) => ({
+    width: '100%',
+    padding: theme.spacing(1),
+}));
 
 const MyDl = () => {
-    const theme = useTheme();
     const { user } = useAuth();
     const dispatch = useDispatch();
 
@@ -19,8 +24,34 @@ const MyDl = () => {
     }, [user, dispatch]);
 
     return (
-        <Card>
-        </Card>
+        <Grid container spacing={3} p={1}>
+            <Grid container item xs={6}>
+                <CardPro>
+                    <Grid item xs={12}>
+                        Khalifa
+                    </Grid>
+                    <Grid item xs={12}>
+                        Bakari
+                    </Grid>
+                    <Grid item xs={12}>
+                        Fumo
+                    </Grid>
+                </CardPro>
+            </Grid>
+            <Grid container item xs={6}>
+                <CardPro>
+                    <Grid item xs={12}>
+                        Real
+                    </Grid>
+                    <Grid item xs={12}>
+                        Slim
+                    </Grid>
+                    <Grid item xs={12}>
+                        Shady
+                    </Grid>
+                </CardPro>
+            </Grid>
+        </Grid>
     );
 }
 
