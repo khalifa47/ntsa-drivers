714675273e6217f701b57d8370f81eddfc6ddc5d
nabcellent <nabcellent.dev@gmail.com>
2022-05-09 06:00:35 +0300

    Implemented: Page - Application for PDL
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/pages/ApplicationForPDL.js
+++ src/pages/ApplicationForPDL.js
@@ -1,13 +1,23 @@
-import { Grid } from '@mui/material';
-import { Newspaper } from '@mui/icons-material';
+import { Grid, Typography } from '@mui/material';
+import { Feed } from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
 
 const ApplicationForPDL = () => {
+    const theme = useTheme();
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <Newspaper/>
-                <h1>Application For Provisional Driving Licence</h1>
+                <Grid item display={'flex'} marginY={'.7rem'}>
+                    <Feed fontSize={'small'}/>
+                    <h1 style={{ marginLeft: '.7rem' }}>Application For Provisional Driving Licence</h1>
+                </Grid>
                 <hr/>
+            </Grid>
+            <Grid item xs={12} marginX={'1rem'} textAlign={'center'}>
+                <Typography variant={'body2'} color={'white'} style={{ backgroundColor: theme.palette.primary.main, padding: '.5rem' }}>
+                    Initial Application
+                </Typography>
             </Grid>
         </Grid>
     );
