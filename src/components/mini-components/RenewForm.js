e4df4b958924904d3e219e866c05409025b783d9
lbnmahs <66216041+lbnmahs@users.noreply.github.com>
2022-05-23 12:50:15 +0300

    Minor changes

--- src/components/mini-components/RenewForm.js
+++ src/components/mini-components/RenewForm.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import RenewForm from '../mini-components/RenewForm'
 
-const Renew = () => {
+const RenewForm = () => {
   return (
     <div className="max-w-xs mx-auto p-5">
       <div className="border w-full bg-green-400 rounded-lg cursor-pointer py-3">
         <h1 className="text-2xl text-center font-semibold text-white">Renew your License</h1>
       </div>
-
-      <RenewForm />
-
     </div>
   )
 }
 
-export default Renew
+export default RenewForm
