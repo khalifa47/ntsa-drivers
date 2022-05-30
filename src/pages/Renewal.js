840a1abf99018317aaa7b8aeac45831c30f90009
nabcellent <nabcellent.dev@gmail.com>
2022-05-30 22:12:15 +0300

    Implements pdl renewal
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/pages/Renewal.js
+++ src/pages/Renewal.js
@@ -1,5 +1,15 @@
 import { Feed, Payments } from "@mui/icons-material";
-import { Box, Divider, FormControlLabel, Grid, LinearProgress, Paper, Radio, RadioGroup, TextField } from "@mui/material";
+import {
+    Box,
+    Divider,
+    FormControlLabel,
+    Grid,
+    LinearProgress,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useAuth } from '../hooks/useAuth';
 import { useTheme } from '@mui/material/styles';
@@ -15,7 +25,7 @@ import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 
 const validationSchema = yup.object({
     id: yup.string()
-        .required('License selection is required.'),
+           .required('License selection is required.'),
     phone: yup.string().test({
         name: 'is-valid-phone',
         message: 'Invalid phone number',
@@ -42,7 +52,9 @@ const Renewal = () => {
             mpesa.onSuccess = async () => {
                 await updateDoc(doc(db, `licenses/${user.uid}/classes`, values.id), {
                     renewedOn: moment().format("MMMM Do YYYY"),
-                    validUntil: values.type === 'pdl' ? moment().add(3, 'months').format("MMMM Do YYYY") : moment().add(3, 'years').format("MMMM Do YYYY")
+                    validUntil: values.type === 'pdl'
+                                ? moment().add(3, 'months').format("MMMM Do YYYY")
+                                : moment().add(3, 'years').format("MMMM Do YYYY")
                 });
             };
 
@@ -53,77 +65,72 @@ const Renewal = () => {
     });
 
     useEffect(() => {
-        const getData = async () => {
-            await getDocs(collection(db, `licenses/${user.uid}/classes`)).then(res => {
-                setExpiredDLs(res.docs.filter(doc => moment(doc.data().validUntil, 'MMMM Do YYYY').isAfter(moment())).map(doc => ({
-                    id: doc.id,
-                    data: doc.data()
-                })));
+        getDocs(collection(db, `licenses/${user.uid}/classes`)).then(res => {
+            setExpiredDLs(res.docs.filter(doc => moment(doc.data().validUntil, 'MMMM Do YYYY').isAfter(moment()))
+                             .map(doc => ({
+                                 id: doc.id,
+                                 data: doc.data()
+                             })));
 
-                setFetchingDL(false);
-            });
-        }
-        getData();
+            setFetchingDL(false);
+        });
     }, [user]);
-
-    console.log(expiredDLs);
 
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
                 <Grid item display={'flex'} marginY={'.7rem'}>
-                    <Feed fontSize={'small'} />
+                    <Feed fontSize={'small'}/>
                     <h1 style={{ marginLeft: '.7rem' }}>Driving Licence Renewal</h1>
                 </Grid>
-                <hr />
+                <hr/>
             </Grid>
 
             {
                 fetchingDL
-                    ? (
-                        <Grid item xs={7} marginX={'auto'} my={5}>
-                            <Box sx={{ width: '100%' }}>
-                                <LinearProgress />
-                            </Box>
+                ? (
+                    <Grid item xs={7} marginX={'auto'} my={5}>
+                        <Box sx={{ width: '100%' }}>
+                            <LinearProgress/>
+                        </Box>
+                    </Grid>
+                ) : (
+                    <>
+                        <Grid item xs={6} marginX={'auto'} textAlign={'center'}>
+                            <Divider light variant={'middle'} sx={{ my: 2 }} color={theme.palette.primary.main}/>
+                            Renew
                         </Grid>
-                    ) : (
-                        <>
-                            <Grid item xs={6} marginX={'auto'} textAlign={'center'}>
-                                <Divider light variant={'middle'} sx={{ my: 2 }} color={theme.palette.primary.main} />
-                                Renew
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Paper sx={{ borderWidth: 1, borderColor: theme.palette.primary.main, paddingY: 3 }}>
-                                    <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}
-                                        padding={'1rem'} component={'form'} onSubmit={formik.handleSubmit}>
-                                        <RadioGroup
-                                            name="id"
-                                            onChange={(event, { value }) => {
-                                                console.log(value);
-                                                formik.setFieldValue('id', value, true);
-                                            }}
-                                        >
-                                            {expiredDLs?.map(dl => (
-                                                <FormControlLabel key={dl.id} value={dl.id} control={<Radio />} label={
-                                                    <CardPro>
-                                                        <Grid item xs={12}>
-                                                            {dl.data.type}
-                                                        </Grid>
-                                                        <Grid item xs={12}>
-                                                            {dl.data.class}
-                                                        </Grid>
-                                                        <Grid item xs={12}>
-                                                            {dl.data.issueDate}
-                                                        </Grid>
-                                                        <Grid item xs={12}>
-                                                            {dl.data.validUntil}
-                                                        </Grid>
-                                                    </CardPro>
-                                                } />
-                                            ))}
-                                        </RadioGroup>
+                        <Grid item xs={12}>
+                            <Paper sx={{ borderWidth: 1, borderColor: theme.palette.primary.main, paddingY: 3 }}>
+                                <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}
+                                      padding={'1rem'} component={'form'} onSubmit={formik.handleSubmit}>
+                                    <RadioGroup
+                                        name="id"
+                                        onChange={(event, value) => {
+                                            formik.setFieldValue('id', value, true);
+                                        }}
+                                    >
+                                        {expiredDLs?.map(dl => (
+                                            <FormControlLabel key={dl.id} value={dl.id} control={<Radio/>} label={
+                                                <CardPro>
+                                                    <Grid item xs={12}>
+                                                        {dl.data.type}
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        {dl.data.class}
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        {dl.data.issueDate}
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        {dl.data.validUntil}
+                                                    </Grid>
+                                                </CardPro>
+                                            }/>
+                                        ))}
+                                    </RadioGroup>
 
-                                        {/* <Grid item xs={12} lg={6}>
+                                    {/* <Grid item xs={12} lg={6}>
                                             <Autocomplete name={'type'} freeSolo
                                                 options={[
                                                     { label: 'PDL', value: 'pdl' },
@@ -155,30 +162,30 @@ const Renewal = () => {
                                                         helperText={formik.touched.class && formik.errors.class} />
                                                 )} />
                                         </Grid> */}
-                                        <Grid item xs={12}>
-                                            <TextField name={'phone'} type={'number'} label="Phone Number" required
-                                                fullWidth
-                                                placeholder={'Phone number'} value={formik.values.phone}
-                                                error={formik.touched.phone && Boolean(formik.errors.phone)}
-                                                helperText={formik.touched.phone && formik.errors.phone}
-                                                onChange={formik.handleChange} />
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <LoadingButton type={'submit'} fullWidth loadingPosition={'end'}
-                                                loading={loading}
-                                                endIcon={<Payments />}
-                                                onClick={() => formik.submitForm()}>
-                                                Pay With MPESA
-                                            </LoadingButton>
-                                        </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField name={'phone'} type={'number'} label="Phone Number" required
+                                                   fullWidth
+                                                   placeholder={'Phone number'} value={formik.values.phone}
+                                                   error={formik.touched.phone && Boolean(formik.errors.phone)}
+                                                   helperText={formik.touched.phone && formik.errors.phone}
+                                                   onChange={formik.handleChange}/>
                                     </Grid>
-                                </Paper>
-                            </Grid>
-                        </>
-                    )
+                                    <Grid item xs={12}>
+                                        <LoadingButton type={'submit'} fullWidth loadingPosition={'end'}
+                                                       loading={loading}
+                                                       endIcon={<Payments/>}
+                                                       onClick={() => formik.submitForm()}>
+                                            Pay With MPESA
+                                        </LoadingButton>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                    </>
+                )
             }
         </Grid>
     );
-}
+};
 
 export default Renewal;
