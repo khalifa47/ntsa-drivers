9e88755082b49c4a780bbf60ca9283964e279eb9
nabcellent <nabcellent.dev@gmail.com>
2022-06-08 11:38:44 +0300

    Fixed formik Validation for license renewal
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/pages/Renewal.js
+++ src/pages/Renewal.js
@@ -1,8 +1,8 @@
 import { Feed, Payments } from "@mui/icons-material";
 import {
     Box,
-    Divider,
-    FormControlLabel,
+    Divider, FormControl,
+    FormControlLabel, FormHelperText,
     Grid,
     LinearProgress,
     Paper,
@@ -25,8 +25,7 @@ import moment from 'moment';
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 
 const validationSchema = yup.object({
-    id: yup.string()
-        .required('License selection is required.'),
+    id: yup.string().required('License selection is required.'),
     phone: yup.string().test({
         name: 'is-valid-phone',
         message: 'Invalid phone number',
@@ -54,8 +53,8 @@ const Renewal = () => {
                 await updateDoc(doc(db, `licenses/${user.uid}/classes`, values.id), {
                     issuedOn: moment().format("MMMM Do YYYY"),
                     validUntil: values.type === 'pdl'
-                        ? moment().add(3, 'months').format("MMMM Do YYYY")
-                        : moment().add(3, 'years').format("MMMM Do YYYY")
+                                ? moment().add(3, 'months').format("MMMM Do YYYY")
+                                : moment().add(3, 'years').format("MMMM Do YYYY")
                 });
             };
 
@@ -68,10 +67,10 @@ const Renewal = () => {
     useEffect(() => {
         getDocs(collection(db, `licenses/${user.uid}/classes`)).then(res => {
             setExpiredDLs(res.docs.filter(doc => moment(doc.data().validUntil, 'MMMM Do YYYY').isBefore(moment()))
-                .map(doc => ({
-                    id: doc.id,
-                    data: doc.data()
-                })));
+                             .map(doc => ({
+                                 id: doc.id,
+                                 data: doc.data()
+                             })));
 
             setFetchingDL(false);
         });
@@ -81,31 +80,32 @@ const Renewal = () => {
         <Grid container spacing={2}>
             <Grid item xs={12}>
                 <Grid item display={'flex'} marginY={'.7rem'}>
-                    <Feed fontSize={'small'} />
+                    <Feed fontSize={'small'}/>
                     <h1 style={{ marginLeft: '.7rem' }}>Driving Licence Renewal</h1>
                 </Grid>
-                <hr />
+                <hr/>
             </Grid>
 
             {
                 fetchingDL
-                    ? (
-                        <Grid item xs={7} marginX={'auto'} my={5}>
-                            <Box sx={{ width: '100%' }}>
-                                <LinearProgress />
-                            </Box>
+                ? (
+                    <Grid item xs={7} marginX={'auto'} my={5}>
+                        <Box sx={{ width: '100%' }}>
+                            <LinearProgress/>
+                        </Box>
+                    </Grid>
+                ) :
+                expiredDLs.length !== 0 ? (
+                    <>
+                        <Grid item xs={6} marginX={'auto'} textAlign={'center'}>
+                            <Divider light variant={'middle'} sx={{ my: 2 }} color={theme.palette.primary.main}/>
+                            Renew
                         </Grid>
-                    ) :
-                    expiredDLs.length !== 0 ? (
-                        <>
-                            <Grid item xs={6} marginX={'auto'} textAlign={'center'}>
-                                <Divider light variant={'middle'} sx={{ my: 2 }} color={theme.palette.primary.main} />
-                                Renew
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Paper sx={{ borderWidth: 1, borderColor: theme.palette.primary.main, paddingY: 3 }}>
-                                    <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}
-                                        padding={'1rem'} component={'form'} onSubmit={formik.handleSubmit}>
+                        <Grid item xs={12}>
+                            <Paper sx={{ borderWidth: 1, borderColor: theme.palette.primary.main, paddingY: 3 }}>
+                                <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}
+                                      padding={'1rem'} component={'form'} onSubmit={formik.handleSubmit}>
+                                    <FormControl error={formik.touched.id && Boolean(formik.errors.id)}>
                                         <RadioGroup
                                             name="id"
                                             row
@@ -114,7 +114,7 @@ const Renewal = () => {
                                             }}
                                         >
                                             {expiredDLs?.map(dl => (
-                                                <FormControlLabel key={dl.id} value={dl.id} control={<Radio />} label={
+                                                <FormControlLabel key={dl.id} value={dl.id} control={<Radio/>} label={
                                                     <CardPro>
                                                         <Grid item xs={12}>
                                                             <strong>Type: </strong>{dl.data.type.toUpperCase()}
@@ -129,35 +129,42 @@ const Renewal = () => {
                                                             <strong>Valid Until: </strong>{dl.data.validUntil}
                                                         </Grid>
                                                     </CardPro>
-                                                } />
+                                                }/>
                                             ))}
                                         </RadioGroup>
+                                        <FormHelperText className={'mt-0'}>
+                                            {formik.touched.id && formik.errors.id}
+                                        </FormHelperText>
+                                    </FormControl>
 
-                                        <Grid item xs={12}>
-                                            <TextField name={'phone'} type={'number'} label="Phone Number" required
-                                                fullWidth
-                                                placeholder={'Phone number'} value={formik.values.phone}
-                                                error={formik.touched.phone && Boolean(formik.errors.phone)}
-                                                helperText={formik.touched.phone && formik.errors.phone}
-                                                onChange={formik.handleChange} />
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <LoadingButton type={'submit'} fullWidth loadingPosition={'end'}
-                                                loading={loading}
-                                                endIcon={<Payments />}
-                                                onClick={() => formik.submitForm()}>
-                                                Pay With MPESA
-                                            </LoadingButton>
-                                        </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField name={'phone'} type={'number'} label="Phone Number" required
+                                                   fullWidth
+                                                   placeholder={'Phone number'} value={formik.values.phone}
+                                                   error={formik.touched.phone && Boolean(formik.errors.phone)}
+                                                   helperText={formik.touched.phone && formik.errors.phone}
+                                                   onChange={formik.handleChange}/>
                                     </Grid>
-                                </Paper>
-                            </Grid>
-                        </>
-                    )
-                        :
-                        <Grid item xs={12}>
-                            <Typography variant='h4' textAlign='center' mx={'auto'} my={4}>DRIVE ON! YOUR DRIVING LICENSES ARE UP TO DATE.</Typography>
+                                    <Grid item xs={12}>
+                                        <LoadingButton type={'submit'} fullWidth loadingPosition={'end'}
+                                                       loading={loading}
+                                                       endIcon={<Payments/>}
+                                                       onClick={() => formik.submitForm()}>
+                                            Pay With MPESA
+                                        </LoadingButton>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
                         </Grid>
+                    </>
+                ) : (
+                    <Grid item xs={12}>
+                        <Typography variant="h4" textAlign="center" mx={'auto'} my={4}>
+                            DRIVE ON! YOUR LICENSES ARE UP TO DATE.
+                        </Typography>
+                    </Grid>
+                )
+
             }
         </Grid>
     );
